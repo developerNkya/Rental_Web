e3d0285f47f23f2c8f16a2bcb022a3a962e7ce2a
developernkya <developernkya@gmail.com>
2024-12-06 14:50:55 +0300

    20241206 | INITIAL TEMPLATE

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -9,32 +9,39 @@ class Login extends BaseController
 {
     public function login()
     {
-        
-        $username = $this->request->getPost('username');
-        $password = $this->request->getPost('password');
-        
-        if (empty($username) || empty($password)) {
+        try {
+            $username = $this->request->getPost('phone_number');
+            $password = $this->request->getPost('password');
+    
+            if (empty($username) || empty($password)) {
+                return $this->response->setJSON([
+                    'success' => false,
+                    'message' => 'Phone number and password are required.',
+                ]);
+            }
+    
+            $userModel = new User();
+            $user = $userModel->where('phone_number', $username)->first();
+    
+            if ($user && password_verify($password, $user['password'])) {
+                return $this->response->setJSON([
+                    'success' => true,
+                    'role'    => $user['role_id'],
+                ]);
+            }
+    
             return $this->response->setJSON([
                 'success' => false,
-                'message' => 'Username and password are required.',
+                'message' => 'Invalid username or password.',
             ]);
-        }
-
-        
-        $userModel = new User();
-        $user = $userModel->where('username', $username)->first();
-
-        if ($user && password_verify($password, $user['password'])) { 
+        } catch (\Throwable $e) { 
+            log_message('error', $e->getMessage()); 
             return $this->response->setJSON([
-                'success' => true,
-                'role'    => $user['role'],
+                'success' => false,
+                'message' => 'An internal server error occurred.',
+                'error'   => $e->getMessage(), 
             ]);
         }
-
-        
-        return $this->response->setJSON([
-            'success' => false,
-            'message' => 'Invalid username or password.',
-        ]);
     }
+    
 }
