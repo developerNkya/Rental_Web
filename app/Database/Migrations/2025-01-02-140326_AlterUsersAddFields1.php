68232eb9e4567f8f66eade8322f3438449eb43a0
developernkya <developernkya@gmail.com>
2025-01-11 16:14:55 +0300

    20250111 | CHANGED MIGRATION NAMES

--- app/Database/Migrations/2025-01-02-140326_AlterUsersAddFields1.php
+++ app/Database/Migrations/2025-01-02-140326_AlterUsersAddFields1.php
@@ -4,7 +4,7 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class AlterUsersAddFields extends Migration
+class AlterUsersAddFields1 extends Migration
 {
     public function up()
     {
